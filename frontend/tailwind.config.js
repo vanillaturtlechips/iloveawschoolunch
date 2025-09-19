2135535925f50ecf8f26723cf01eed9029c01950
vanillaturtlechips <2220110150@office.kopo.ac.kr>
2025-09-19 12:08:57 +0900

    front 추가

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 👈 이 부분을 수정/추가해주세요.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
