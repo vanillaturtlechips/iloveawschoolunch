9b08b5e4a6f2496b73e235f8150fa91bd72b59ae
vanillaturtlechips <2220110150@office.kopo.ac.kr>
2025-09-22 17:31:27 +0900

    test

--- backend/asgi.py
+++ backend/asgi.py
@@ -13,6 +13,6 @@ from django.core.asgi import get_asgi_application
 
 ## backend.settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_asgi_application()
